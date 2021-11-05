13884e7487c862455fe12f2d489469c3976dafe4
FelipeGualberto <fgualberto.santos@gmail.com>
2021-11-05 12:51:38 -0300

    First Commit

---
+++ android/app/src/main/kotlin/com/example/instrucao_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.instrucao_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+//Colors
+const kPrimaryColor = Color(0xFF0052CC);
+const kPrimaryDarkColor = Color(0xFF091E42);
+const kSupportColor = Color(0xFFE5E5E5);
+const kTitleTextColor = Color(0xFF2A2A2A);
+const kBodyTextColor = Color(0xFF4B4B4B);
+const kTextLightColor = Color(0xFF959595);
+const kDeathColor = Color(0xFFFF4848);
+const kBackgroundColor = Color(0xFFF4F5F7);
+
+final kShadowColor = Color(0xFFB7B7B7).withOpacity(.16);
+final kActiveShadowColor = Color(0xFFB7B7B7).withOpacity(.15);
+
+
+//Text Style
+const kHeadingTextStyle1 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: kPrimaryDarkColor
+);
+
+const kHeadingTextStyle2 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    color: kPrimaryDarkColor
+);
+
+const kSubTextStyle = TextStyle(
+  fontSize: 14,
+  color: kTextLightColor,
+);
+const kTitleTextStyle = TextStyle(
+  fontSize: 18,
+  color: kTitleTextColor,
+  fontWeight: FontWeight.bold,
+);
